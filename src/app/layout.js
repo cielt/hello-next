65da9e89127975564b32d0e9a0a068b31094d185
leslie tu <chiayin.l.tu@gmail.com>
2026-09-12 18:31:48 -0400

    [init] upgrade next js

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,11 +3,16 @@ import React from 'react';
 import './styles.css';
 
 function RootLayout({ children }) {
+  const timestamp = new Date().toLocaleString();
+  console.log('layout rendered');
+
   return (
     <html lang="en">
       <body>
         {children}
-
+        <footer>
+          <p>Page rendered on {timestamp}</p>        
+        </footer>
       </body>
     </html>
   );
